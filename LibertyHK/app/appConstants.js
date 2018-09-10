1c4b9994b4a6e0ea4c4ccefc1ef3c043aa545acb
chezhiangit <chezhian.p@gmail.com>
2018-09-10 09:29:29 +0530

    created app constants file

---
+++ LibertyHK/app/appConstants.js
@@ -0,0 +1,22 @@
+/*
+This file is to maintain following, which to be used accross app.
+  color constants
+  font size
+  padding and margin size
+
+*/
+
+const AppConstant = {
+  colors:{
+
+  },
+  fontSize:{
+
+  },
+  spacing:{
+
+  }
+
+}
+
+export default AppConstant;
